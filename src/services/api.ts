ec5da897a852871a636087b92215892ad434ca49
Beatriz Motta <bigobaldohoran@gmail.com>
2024-02-07 00:58:39 -0300

    fix: change baseURL to current correct link

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://backendd-vk3y.onrender.com",
+    baseURL: "https://backendd-vk3y.onrender.com/api/v1",
 })
 
 export default api;
