3ce1ead7c604a2a2e71239aa61cfec2dcb236396
Beatriz Motta <mbmbv@ic.ufal.br>
2024-02-13 17:39:17 -0300

    fix: merging changes from repo "tryingdeploy"

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -3,28 +3,53 @@ import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 import { Button } from "@chakra-ui/react";
 import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function NavBar() {
+    const [token, setToken] = useState<string>(() => localStorage.getItem('token') || '');
+    const [redirected, setRedirected] = useState(false);
+    const location = useLocation();
 
-    const location = useLocation(); 
+    const logout = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user_id');
+        window.location.href = '/';
+        
+    };
+    const validToken = async () => {
+        try {
+            
+            
+            const response = await axios.post('https://backendd-vk3y.onrender.com/api/v1/authentication/token/verify/', { token });
+           
+            return response // 
+        } catch (error) {
+            console.error('Erro ao validar o token:', error);
+            return false;
+        }
+    };
+    
 
     return (
         <div className={styles.container}
-        style={location.pathname === "/login" ? {position: "absolute"} : {position: "unset"}}
+            style={location.pathname === "/login" ? { position: "absolute" } : { position: "unset" }}
         >
-            <Link to="/" className={styles.container_logo}> 
-                    <Logo className={styles.logo} />
+            <Link to="/" className={styles.container_logo}>
+                <Logo className={styles.logo} />
             </Link>
-            {location.pathname !== "/login" && 
-                <Link to="/login">
-                    <Button 
+            {token && location.pathname !== "/login" &&
+                <Button
+                    onClick={logout}
                     bg="green.50"
                     colorScheme="green"
                     style={{
                         padding: "2px 12px"
                     }}
-                    >Login</Button>
-                </Link>
+                    type="submit"
+                >
+                    Logout
+                </Button>
             }
         </div>
     );
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,23 +2,65 @@ import wave from "assets/images/wave-1.svg";
 import styles from "./styles.module.css";
 import { Link } from "react-router-dom";
 import { Card, VStack, Input, CardBody, Avatar, Button, ButtonGroup } from "@chakra-ui/react";
+import axios from "axios";
+import { useState, createContext, useContext } from "react";
 
 
 function Login() {
+
+    
+    const [post, setPost] = useState({
+        username:'',
+        password:''
+    
+    })
+    const [token, setToken] = useState<string>('');
+    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setPost((prevPost) => ({
+            ...prevPost,
+            [name]: value,
+        }));
+    };
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        
+    
+        axios.post('https://backendd-vk3y.onrender.com/api/v1/authentication/token/', post)
+            .then(response => {
+                const  token  = response.data.access; 
+                console.log(token)
+                setToken(token);
+                localStorage.setItem('token', token);
+                const payload = JSON.parse(atob(token.split('.')[1]));
+                
+                const userId = payload.user_id;
+                localStorage.setItem('user_id', userId);
+                
+        
+                
+                
+                window.location.href = '/dashboard';
+            })
+            .catch(err => console.log(err));
+    }
     return (
         <div className={styles.container} 
         style={{
             backgroundImage: `url(${wave})`
         }}>
+            <form onSubmit={handleSubmit}>
             <Card className={styles.login_card} w="lg">
                 <CardBody className={styles.login_card_body}>
                     <Avatar size="2xl" />
                     <VStack spacing="17px" mt="40px" w="fit-content">
-                        <Input type="email" placeholder="Usuário" />
-                        <Input type="password" placeholder="Senha" />
+                        
+                            <Input type="text" onChange={handleInput} placeholder="Usuário" name="username" />
+                            <Input type="password" onChange={handleInput} placeholder="Senha" name="password" />
+                        
                         <ButtonGroup>
                             <Link to="/">
-                                <Button 
+                                <Button
                                 color="white.50" 
                                 bg="pink.50" 
                                 _hover={{bgColor: "pink.100"}} 
@@ -26,18 +68,19 @@ function Login() {
                                     VOLTAR
                                 </Button>
                             </Link>
-                            <Link to="/dashboard">
+                            {/* <Link to="/dashboard"> */}
                                 <Button
+                                type="submit"
                                 color="white.50"
                                 bg="green.50"
                                 colorScheme="green"
                                 >ENVIAR</Button>
-                            </Link>
+                            {/* </Link> */}
                         </ButtonGroup>
                     </VStack>
                 </CardBody>
             </Card>
-            
+            </form>
             
         </div>
     );
