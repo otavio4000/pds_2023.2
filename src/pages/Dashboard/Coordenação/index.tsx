cf1341ebb480ff0d9ee1dabb79776b8c8336d45d
Brunot101 <contato.brunoluiz101@gmail.com>
2024-03-12 11:09:57 -0300

    Adicionando card nas denúncias

--- src/pages/Dashboard/Coordenação/index.tsx
+++ src/pages/Dashboard/Coordenação/index.tsx
@@ -2,12 +2,32 @@ import NavBar from "components/NavBar";
 import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
+import styles from "./styles.module.css";
+import v_fisica from "../../../assets/images/icons8-soco-60.png";
+import bulliyng from "../../../assets/images/icons8-rufia-60.png";
+import v_verbal from "../../../assets/images/icons8-homem-xingando-60.png";
+import assedio from "../../../assets/images/icons8-agressor-sexual-60.png"
+import { format } from 'date-fns';
+
+declare module 'date-fns' {
+  interface FormatOptions {
+    timeZone?: string;
+  }
+}
+
+
 export type Title = {
     matricula: number;
     relato: string;
+    recorrencia:string;
     id: number;
     body: string;
     title: string;
+    v_fisica: string;
+    v_verbal: string;
+    bullying: string;
+    assedio: string;
+    data_ocorrido: string;
   };
 
 
@@ -20,8 +40,13 @@ const Coordenacao = () => {
     
     const location = useLocation();
     const path = location.pathname;
-
-
+    function formatDate(dateTime: string): string {
+      const newDate = new Date(dateTime);
+     
+      return format(newDate, 'dd/MM/yyyy ', {
+        timeZone: 'America/Sao_Paulo', 
+      });
+    }
 
 
     useEffect(() => {
@@ -50,15 +75,64 @@ const Coordenacao = () => {
           });
       }, [navigate]);
     return (
-        // Colocar o modelo do card aqui
-         <div className="cardaqui">
-                {titles ? titles.map((id) => {
-                return <p key={id.id}>{id.relato}</p>;
-                }) : null}
-            </div> 
+        
+        
+        <div className={styles.cardaqui}>
+          
+      {titles
+        ? titles.map((id) => (
+            <div key={id.id} className={styles.card}>
+
+              <div className={styles.cardIcons}>
+                {id.v_fisica === 'yes' && (
+                  <img
+                    src={v_fisica}
+                    
+                    className={styles.icone}
+                  />
+                )}
+                {id.v_verbal=== 'yes' && (
+                  <img
+                    src={v_verbal}
+                    
+                    className={styles.icone}
+                  />
+                )}
+                {id.bullying=== 'yes' && (
+                  <img
+                    src={bulliyng}
+                    
+                    className={styles.icone}
+                  />
+                )}
+                {id.assedio=== 'yes' && (
+                  <img
+                    src={assedio}
+                    
+                    className={styles.icone}
+                  />
+                )}
+              
+              </div>
+                <div className={styles.content}>
+                  <p><strong>Matricula: </strong>{id.matricula}</p>
+                  <p> <strong>Recorrencia: </strong>{id.recorrencia}</p>
+                  <p> <strong>Data: </strong> {formatDate(id.data_ocorrido)}</p>
+                
+                </div>
+                
+              
+              
+              
+              
+            </div>
+          ))
+        : null}
+    </div> 
         
          
     )
 }
 
 export default Coordenacao;
+
