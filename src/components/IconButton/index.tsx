308f93437c5585f1202f0408c3a41233a0af891f
Beatriz Motta <mbmbv@ic.ufal.br>
2024-03-21 01:28:47 -0300

    feat: add possibility of adding style

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -1,19 +1,18 @@
-import { ReactComponent as MenuIcon } from "assets/icons/hamburger-menu-svgrepo-com.svg";
-import { ElementType } from "react";
-import { ReactComponent as BackIcon } from "assets/icons/chevron-left-svgrepo-com.svg";
+import { CSSProperties, ElementType } from "react";
 import styles from "./styles.module.css";
 import { Button } from "@chakra-ui/react";
 
 const IconButton = (props: {
     icon: ElementType,
-    handleClick: () => void
+    handleClick: () => void,
+    style?: CSSProperties 
 }) => {
 
-    const { icon: Icon, handleClick } = props; 
+    const { icon: Icon, handleClick, style } = props; 
 
     return (
         <div className={styles.container}>
-            <Button variant="icon" className={styles.button} onClick={handleClick}>
+            <Button variant="icon" className={styles.button} onClick={handleClick} style={style}>
                 <Icon className={styles.button_icon} /> 
             </Button>
         </div>
