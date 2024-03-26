751048916d09fa4bc910f2a80c25d7de68519d12
Beatriz Motta <mbmbv@ic.ufal.br>
2024-03-26 00:38:26 -0300

    feat: add wrong password response and loading skeletons for every loading screen

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,19 @@
 import wave from "assets/images/wave-1.svg";
 import styles from "./styles.module.css";
 import { Link, useNavigate } from "react-router-dom";
-import { Card, VStack, Input, CardBody, Avatar, Button, ButtonGroup } from "@chakra-ui/react";
+import {
+    Card,
+    VStack,
+    Input,
+    CardBody,
+    Avatar,
+    Button,
+    ButtonGroup,
+    Alert,
+    AlertIcon,
+    AlertTitle,
+    AlertDescription,
+} from "@chakra-ui/react";
 import api from "services/api";
 import { useState, createContext, useContext } from "react";
 
@@ -9,13 +21,14 @@ import { useState, createContext, useContext } from "react";
 function Login() {
 
     const navigate = useNavigate();
-    
+
     const [post, setPost] = useState({
-        username:'',
-        password:''
+        username: '',
+        password: ''
     })
     const [token, setToken] = useState<string>('');
     const [isLoading, setIsLoading] = useState(false);
+    const [errorInLoging, setErrorInLoging] = useState("");
 
     const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target;
@@ -28,23 +41,16 @@ function Login() {
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
-        // APENAS PARA MOCKAR A AUTORIZAÇÃO:
-        if (post.username == "pai" && post.password == "pai") {
-            localStorage.setItem("user_type", "pai");
-            navigate("/dashboard");
-        }
-
-        
         setIsLoading(true);
+
         api.post('/authentication/token/', post)
             .then(response => {
                 setIsLoading(false);
-                console.log(response);
 
-                const token  = response.data.access; 
+                const token = response.data.access;
                 setToken(token);
                 localStorage.setItem('token', token);
-                
+
                 const payload = JSON.parse(atob(token.split('.')[1]));
                 const userId = payload.user_id;
                 localStorage.setItem('user_id', userId);
@@ -52,47 +58,56 @@ function Login() {
                 navigate("/dashboard");
             })
             .catch(err => {
+                setErrorInLoging(err.response.data.detail)
                 setIsLoading(false);
                 console.log(err);
             });
     }
     return (
-        <div className={styles.container} 
-        style={{
-            backgroundImage: `url(${wave})`
-        }}>
+        <div className={styles.container}
+            style={{
+                backgroundImage: `url(${wave})`
+            }}>
             <form onSubmit={handleSubmit}>
-            <Card className={styles.login_card} w="lg">
-                <CardBody className={styles.login_card_body}>
-                    <Avatar size="2xl" />
-                    <VStack spacing="17px" mt="40px" w="fit-content">
-                        
+                <Card className={styles.login_card} w="lg">
+                    <CardBody className={styles.login_card_body}>
+                        <Avatar size="2xl" />
+                        <VStack spacing="17px" mt="40px" w="fit-content">
+
                             <Input type="text" onChange={handleInput} placeholder="Usuário" name="username" />
                             <Input type="password" onChange={handleInput} placeholder="Senha" name="password" />
-                        
-                        <ButtonGroup>
-                            <Link to="/">
+
+                            {
+                                errorInLoging != "" && 
+                                <Alert status='error'>
+                                    <AlertIcon />
+                                    <AlertDescription> {errorInLoging} </AlertDescription>
+                                </Alert>
+                            }
+
+                            <ButtonGroup>
+                                <Link to="/">
+                                    <Button
+                                        color="white.50"
+                                        bg="pink.50"
+                                        _hover={{ bgColor: "pink.100" }}
+                                        _active={{ bgColor: "pink.200" }}>
+                                        VOLTAR
+                                    </Button>
+                                </Link>
                                 <Button
-                                color="white.50" 
-                                bg="pink.50" 
-                                _hover={{bgColor: "pink.100"}} 
-                                _active={{bgColor: "pink.200"}}>
-                                    VOLTAR
-                                </Button>
-                            </Link>
-                                <Button
-                                type="submit"
-                                color="white.50"
-                                bg="green.50"
-                                colorScheme="green"
-                                isLoading={isLoading}
+                                    type="submit"
+                                    color="white.50"
+                                    bg="green.50"
+                                    colorScheme="green"
+                                    isLoading={isLoading}
                                 >ENVIAR</Button>
-                        </ButtonGroup>
-                    </VStack>
-                </CardBody>
-            </Card>
+                            </ButtonGroup>
+                        </VStack>
+                    </CardBody>
+                </Card>
             </form>
-            
+
         </div>
     );
 }
